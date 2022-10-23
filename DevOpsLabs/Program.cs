10b4400ccad102273e4cdae9f4ac29192fd66026
L00177579 <L00177579@atu.ie>
2022-10-23 11:35:31 +0100

    feat: Added change via Vim

--- DevOpsLabs/Program.cs
+++ DevOpsLabs/Program.cs
@@ -10,7 +10,7 @@ namespace DevOpsLabs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            Console.WriteLine("Test Vim");
         }
     }
 }
