8ceab5c55f8f012a9b5f1a11ec6ddbc275c0379a
L00177579 <L00177579@atu.ie>
2022-10-23 11:29:20 +0100

    feat: Added some basic classes, made a main entry point

--- DevOpsLabs/BusinessLogic.cs
+++ DevOpsLabs/BusinessLogic.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace DevOpsLabs
 {
-    internal class Program
+    internal class BusinessLogic
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Test");
-        }
     }
 }
