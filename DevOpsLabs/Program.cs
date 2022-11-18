fe5767262ae1efbcc2fd5c9be4ee9a968e5729f4
L00177579 <L00177579@atu.ie>
2022-11-18 19:47:02 +0000

    feat: changed to console project, added commandline args

--- DevOpsLabs/Program.cs
+++ DevOpsLabs/Program.cs
@@ -8,5 +8,21 @@ namespace DevOpsLabs
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            BusinessLogic businessLogic = new BusinessLogic();
+            if (args[0] == "mutliply")
+            {
+                Console.WriteLine(businessLogic.Addition(double.Parse(args[1]), double.Parse(args[2])));
+            }
+            else if (args[0] == "addition")
+            {
+                Console.WriteLine(businessLogic.Multiplication(double.Parse(args[1]), double.Parse(args[2])));
+            }
+            else
+            {
+                Console.WriteLine("Invalid arguments");
+            }
+        }
     }
 }
